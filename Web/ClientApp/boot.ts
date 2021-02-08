d6dbc091080c78beddfe3c7b35393eef93c48d63
Keith Tuttle <klktj01@gmail.com>
2021-02-08 13:27:59 -0500

    Add project files.

---
+++ Web/ClientApp/boot.ts
@@ -0,0 +1,30 @@
+import './css/site.css';
+import 'bootstrap';
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin, LayoutPlugin, ModalPlugin, CardPlugin, VBScrollspyPlugin, DropdownPlugin, TablePlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '/', component: require('./components/home/home.vue.html') },
+    { path: '/counter', component: require('./components/counter/counter.vue.html') },
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+];
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ModalPlugin);
+Vue.use(CardPlugin);
+Vue.use(VBScrollspyPlugin);
+Vue.use(DropdownPlugin);
+Vue.use(TablePlugin);
+
+new Vue({
+    el: '#app-root',
+    router: new VueRouter({ mode: 'history', routes: routes }),
+    render: h => h(require('./components/app/app.vue.html'))
+});
---
+++ Web/ClientApp/components/fetchdata/fetchdata.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
+
+@Component
+export default class FetchDataComponent extends Vue {
+    forecasts: WeatherForecast[] = [];
+    dateString = "Date";
+    text = "test";
+
+    mounted() {
+        fetch('api/SampleData/WeatherForecasts')
+            .then(response => response.json() as Promise<WeatherForecast[]>)
+            .then(data => {
+                this.forecasts = data;
+            });
+    }
+}
